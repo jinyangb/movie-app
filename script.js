5f3437d6ab8b6617342db4c230c006fc518ee639
jinyangb <your_email@example.com>
2022-01-25 17:05:41 -0500

    added api

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 const API_URL =
   'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=563ffa95d474cf0b16907e7a19643411&page=1'
-
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
-
 const SEARCH_API =
-  'https://api.themoviedb.org/3/discover/movie?api_key=563ffa95d474cf0b16907e7a19643411&query="'
+  'https://api.themoviedb.org/3/search/movie?api_key=563ffa95d474cf0b16907e7a19643411&query="'
 
 const main = document.getElementById('main')
 const form = document.getElementById('form')
@@ -30,16 +28,16 @@ function showMovies(movies) {
     movieEl.classList.add('movie')
 
     movieEl.innerHTML = `
-              <img src="${IMG_PATH + poster_path}" alt="${title}">
-              <div class="movie-info">
-            <h3>${title}</h3>
-            <span class="${getClassByRate(vote_average)}">${vote_average}</span>
-              </div>
-              <div class="overview">
-            <h3>Overview</h3>
-            ${overview}
-          </div>
-          `
+            <img src="${IMG_PATH + poster_path}" alt="${title}">
+            <div class="movie-info">
+          <h3>${title}</h3>
+          <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+          <h3>Overview</h3>
+          ${overview}
+        </div>
+        `
     main.appendChild(movieEl)
   })
 }
